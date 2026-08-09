0d696b956c6e0cfc935ff896ee2dd361b782011f
Qsdc05 <2438281433@qq.com>
2026-08-09 14:13:36 +0800

    chore: remove project files

--- src-env.d.ts
+++
@@ -1,3 +0,0 @@
-interface ImportMetaEnv { readonly VITE_API_URL?: string }
-interface ImportMeta { readonly env: ImportMetaEnv }
-/// <reference types="vite/client" />
--- src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-interface ImportMetaEnv { readonly VITE_API_URL?: string; readonly PROD: boolean }
-interface ImportMeta { readonly env: ImportMetaEnv }
-/// <reference types="vite/client" />
